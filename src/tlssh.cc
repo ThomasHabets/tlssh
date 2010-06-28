281be9650ff4a4554a6cac36c212a710469e3c13
Thomas Habets <thomas@habets.pp.se>
2010-06-28 22:07:13 +0200

    tlssh -V added

--- src/tlssh.cc
+++ src/tlssh.cc
@@ -196,7 +196,14 @@ usage(int err)
 void
 print_version()
 {
-	printf("tlssh version", VERSION);
+	printf("tlssh %s\n"
+	       "Copyright (C) 2010 Thomas Habets <thomas@habets.pp.se>\n"
+	       "License GPLv2: GNU GPL version 2 or later "
+	       "<http://gnu.org/licenses/gpl-2.0.html>\n"
+	       "This is free software: you are free to change and "
+	       "redistribute it.\n"
+	       "There is NO WARRANTY, to the extent permitted by law.\n",
+	       VERSION);
 }
 
 /**
